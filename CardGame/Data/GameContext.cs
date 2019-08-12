b833e52921ef39b321f2e13f4923ee21c9c36be6
Mattias Berglund <berglund92@outlook.com>
2019-08-12 16:54:49 +0200

    Sql-logger implemented

--- CardGame/Data/GameContext.cs
+++ CardGame/Data/GameContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using CardGame.ClassLibrary;
+using Microsoft.Extensions.Logging; // Log sql-queries
+using Microsoft.Extensions.Logging.Console; // Log sql-queries in console
 
 namespace CardGame.Data
 {
@@ -8,10 +10,20 @@ namespace CardGame.Data
         public DbSet<User> Users { get; set; }
         public DbSet<UserScore> UserScores { get; set; }
 
+        // Log sql-queries to console
+        public static readonly LoggerFactory MyConsoleLogger
+            = new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category, level)
+                    => category == DbLoggerCategory.Database.Command.Name // only show sql-commands
+                && level == LogLevel.Information, true) // level of detail (just basic information)
+            });
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                //.UseLoggerFactory(MyConsoleLogger) // Log sql-queries
+                .UseSqlServer(
                 "Server = (localdb)\\mssqllocaldb; Database = CardGame");
         }
     }
